454918b3986ce88f862efeedcba23298ead69ae1
Jerry <zhangji87@gmail.com>
2024-05-03 14:02:10 +0800

    SICP: sqrt

--- sicp-rkt/hello.rkt
+++ sicp-rkt/hello.rkt
@@ -22,3 +22,20 @@
       x))
 
 (define (>= x y) (or (> x y) (= x y)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
