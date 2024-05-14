2b1df79c9c82b036ab7677def4aa2cf9043fd48a
Jerry <zhangji87@gmail.com>
2024-05-14 09:34:45 +0800

    SICP: list ops

--- sicp-rkt/list-ops.rkt
+++ sicp-rkt/list-ops.rkt
@@ -21,3 +21,26 @@
   (if (null? list1)
       list2
       (cons (car list1) (append (cdr list1) list2))))
+
+(define (last-pair items)
+  (if (or (null? items) (null? (cdr items)))
+      items
+      (last-pair (cdr items))))
+
+(define (reverse items)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (cons (car rest) result) (cdr rest))))
+  (iter nil items))
+
+(define (same-parity a . w)
+  (define valid? (if (even? a) even? odd?))
+  (define (iter result rest)
+    (cond ((null? rest) result)
+          ((valid? (car rest))
+           (iter (append result (list (car rest)))
+                 (cdr rest)))
+          (else
+           (iter result (cdr rest)))))
+  (iter (list a) w))
