1c0d75ad2cba872bb929c432b200191d6991fc92
Jerry <zhangji87@gmail.com>
2024-05-25 20:10:28 +0800

    SICP: make account

--- sicp-rkt/hello.rkt
+++ sicp-rkt/hello.rkt
@@ -181,3 +181,19 @@
 (define (sqrt2 x)
   (newtons-method
    (lambda (y) (- (square y) x )) 1.0))
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request: MAKE-ACCOUNT"
+                       m))))
+  dispatch)
