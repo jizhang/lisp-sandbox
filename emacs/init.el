1db48de53dd81af2caa105a0f3ee67ebd41b3838
Jerry <zhangji87@gmail.com>
2024-07-17 18:21:52 +0800

    Use package

--- emacs/init.el
+++ emacs/init.el
@@ -3,5 +3,25 @@
 	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 
+(use-package markdown-mode
+  :ensure t)
+
+(use-package editorconfig
+  :ensure t
+  :config (editorconfig-mode 1))
+
 (push '(fullscreen . maximized) default-frame-alist)
 (tool-bar-mode -1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(editorconfig markdown-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
