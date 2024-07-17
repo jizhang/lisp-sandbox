5644b8c2244baac1bd11d64d64d4d7ed5f12f6a6
Jerry <zhangji87@gmail.com>
2024-07-17 13:51:52 +0800

    Hello c

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa-stable"
-	       . "https://stable.melpa.org/packages/"))
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 
 (push '(fullscreen . maximized) default-frame-alist)
