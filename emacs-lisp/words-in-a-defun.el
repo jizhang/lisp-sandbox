43e385f346d17ac12e8cdbdecf84b47d7d8a85a4
Jerry <zhangji87@gmail.com>
2024-08-28 09:26:49 +0800

    Count words in defun

--- emacs-lisp/words-in-a-defun.el
+++ emacs-lisp/words-in-a-defun.el
@@ -1,3 +1,38 @@
+(defun count-words-in-defun ()
+  (beginning-of-defun)
+  (let ((count 0)
+        (end (save-excursion (end-of-defun) (point))))
+    (while (and (< (point) end)
+                (re-search-forward "\\(\\w\\|\\s_\\)+[^ \t\n]*[ \t\n]*" end t))
+      (setq count (1+ count)))
+    count))
+
+(defun lengths-list-file (filename)
+  (message "Working on `%s' ..." filename)
+  (save-excursion
+    (let ((buffer (find-file-noselect filename))
+          lengths-list)
+      (set-buffer buffer)
+      (setq buffer-read-only t)
+      (widen)
+      (goto-char (point-min))
+      (while (re-search-forward "^(defun" nil t)
+        (setq lengths-list (cons (count-words-in-defun) lengths-list)))
+      (kill-buffer buffer)
+      lengths-list)))
+
+(defun lengths-list-many-files (list-of-files)
+  (let (lengths-list)
+    (dolist (filename list-of-files lengths-list)
+      (setq lengths-list (append lengths-list
+                                 (lengths-list-file (expand-file-name filename)))))))
+
+(defun recursive-lengths-list-many-files (list-of-files)
+  (if list-of-files
+      (append (lengths-list-file (expand-file-name (car list-of-files)))
+              (recursive-lengths-list-many-files (cdr list-of-files)))
+    nil))
+
 (defun files-in-below-directory (dir)
   (let (el-files
         (files (directory-files-and-attributes dir t)))
