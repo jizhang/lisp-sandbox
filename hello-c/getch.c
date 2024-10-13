3f2e0f5c622df8fed8f8f808a544096a82392fb8
Jerry <zhangji87@gmail.com>
2024-10-13 09:43:06 +0800

    Split files

---
+++ hello-c/getch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+  return bufp > 0 ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+  if (bufp < BUFSIZE) {
+    buf[bufp++] = c;
+  } else {
+    printf("ungetch: too many characters\n");
+  }
+}
