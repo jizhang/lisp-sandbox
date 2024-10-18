da9032641aebef06c8c0eb65e70ceac4ed7db0bf
Jerry <zhangji87@gmail.com>
2024-10-18 09:19:11 +0800

    Use struct pointer

--- hello-c/count_keywords.c
+++ hello-c/count_keywords.c
@@ -11,7 +11,7 @@ struct key {
 };
 
 int getword(char *, int);
-int binsearch(char *, struct key *, int);
+struct key *binsearch(char *, struct key *, int);
 
 struct key keytab[] = {
   "auto", 0,
@@ -32,33 +32,36 @@ int main() {
   char word[MAXWORD];
   while (getword(word, MAXWORD) != EOF) {
     if (isalpha(word[0])) {
-      int n = binsearch(word, keytab, NKEYS);
-      if (n != -1) {
-        ++keytab[n].count;
+      struct key *p = binsearch(word, keytab, NKEYS);
+      if (p != NULL) {
+        ++p->count;
       }
     }
   }
 
-  for (int n = 0; n < NKEYS; ++n) {
-    if (keytab[n].count > 0) {
-      printf("%4d %s\n", keytab[n].count, keytab[n].word);
+  for (struct key *p = keytab; p < keytab + NKEYS; ++p) {
+    if (p->count > 0) {
+      printf("%4d %s\n", p->count, p->word);
     }
   }
 
   return 0;
 }
 
-int binsearch(char *word, struct key tab[], int n) {
-  int low = 0;
-  int high = n - 1;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    int cond = strcmp(word, tab[mid].word);
-    if (cond < 0) high = mid - 1;
+struct key *binsearch(char *word, struct key tab[], int n) {
+  if (n == 0) return NULL;
+
+  struct key *low = &tab[0];
+  struct key *high = &tab[n];
+  while (low < high) {
+    struct key *mid = low + (high - low) / 2;
+    int cond = strcmp(word, mid->word);
+    if (cond < 0) high = mid;
     else if (cond > 0) low = mid + 1;
     else return mid;
   }
-  return -1;
+
+  return NULL;
 }
 
 int getword(char *word, int lim) {
