da039240ede4ed2e1405b168bc23b4871e254724
Jerry <zhangji87@gmail.com>
2024-12-06 15:55:44 +0800

    Use cl-defstruct

--- emacs-lisp/doubly-linked-list.el
+++ emacs-lisp/doubly-linked-list.el
@@ -1,54 +1,49 @@
-(defun make-dnode (value &optional previous next)
-  (record 'dlist-node value previous next))
+(require 'cl-lib)
 
-(defun set-previous-dnode (dnode previous) (aset dnode 2 previous))
-(defun set-next-dnode (dnode next) (aset dnode 3 next))
-(defun dnode-value (dnode) (aref dnode 1))
-(defun previous-dnode (dnode) (aref dnode 2))
-(defun next-dnode (dnode) (aref dnode 3))
+(cl-defstruct dnode (value 0) previous next)
+
+(cl-defstruct (dlist (:constructor make-dlist-0))
+  head tail)
 
 (defun make-dlist ()
-  (let* ((head (make-dnode 0))
-         (tail (make-dnode 0 head)))
-    (set-next-dnode head tail)
-    (record 'dlist head tail)))
-
-(defun dlist-head (dlist) (aref dlist 1))
-(defun dlist-tail (dlist) (aref dlist 2))
+  (let* ((head (make-dnode))
+         (tail (make-dnode :previous head)))
+    (setf (dnode-next head) tail)
+    (make-dlist-0 :head head :tail tail)))
 
 (defun add-to-dlist (dlist element)
   "Append to the end."
   (let* ((tail (dlist-tail dlist))
-         (prev (previous-dnode tail))
-         (node (make-dnode element prev tail)))
-    (set-next-dnode prev node)
-    (set-previous-dnode tail node)))
+         (prev (dnode-previous tail))
+         (node (make-dnode :value element :previous prev :next tail)))
+    (setf (dnode-next prev) node)
+    (setf (dnode-previous tail) node)))
 
 (defun remove-from-dlist (dlist)
   "Remove first."
   (let* ((head (dlist-head dlist))
-         (node (next-dnode head)))
+         (node (dnode-next head)))
     (when (eq node (dlist-tail dlist))
       (error "List is empty"))
-    (let ((next (next-dnode node)))
-      (set-next-dnode head next)
-      (set-previous-dnode next head))
+    (let ((next (dnode-next node)))
+      (setf (dnode-next head) next)
+      (setf (dnode-previous next) head))
     (dnode-value node)))
 
 (defun clear-dlist (dlist)
   (let ((head (dlist-head dlist))
         (tail (dlist-tail dlist)))
-    (set-next-dnode head tail)
-    (set-previous-dnode tail head)))
+    (setf (dnode-next head) tail)
+    (setf (dnode-previous tail) head)))
 
 (defun dlist-as-list (dlist)
   (let* ((head (dlist-head dlist))
          (tail (dlist-tail dlist))
-         (node (previous-dnode tail))
+         (node (dnode-previous tail))
          (result nil))
     (while (not (eq node head))
       (push (dnode-value node) result)
-      (setq node (previous-dnode node)))
+      (setq node (dnode-previous node)))
     result))
 
 (let ((dlist (make-dlist)))
@@ -62,5 +57,4 @@
 
 ;; TODO
 ;; Add and remove by index.
-;; Method dispatch.
-;; Use `cl-defstruct' or `defclass'.
+;; Method dispatch. https://nullprogram.com/blog/2018/02/14/
