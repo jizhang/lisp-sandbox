f8b3622acef898503660c769c28291e88422dabc
Jerry <zhangji87@gmail.com>
2024-12-08 11:55:42 +0800

    Add and remove at index (#1)
    * Add at
    
    * Extract insert-dnode
    
    * Remove at
    
    * Remove last
    
    * Fix symbol in macro

--- emacs-lisp/doubly-linked-list-tests.el
+++ emacs-lisp/doubly-linked-list-tests.el
@@ -1,18 +1,44 @@
 (require 'doubly-linked-list)
 
+(defmacro assert-error (form message)
+  (let ((err (make-symbol "err")))
+    `(let ((,err (should-error ,form)))
+       (should (equal (cadr ,err) ,message)))))
+
 (ert-deftest dlist-test-add ()
   (let ((dlist (make-dlist)))
     (should (equal (dlist-as-list dlist) nil))
 
-    (add-to-dlist dlist 1)
+    (should (equal (add-to-dlist dlist 1) t))
     (should (equal (dlist-as-list dlist) '(1)))
 
     (add-to-dlist dlist 2)
     (add-to-dlist dlist 3)
     (should (equal (dlist-as-list dlist) '(1 2 3)))))
 
-(ert-deftest dlist-test-remove ()
+(ert-deftest dlist-test-add-at ()
   (let ((dlist (make-dlist)))
+    (should (equal (add-first-to-dlist dlist 1) t))
+    (should (equal (dlist-as-list dlist) '(1)))
+
+    (add-first-to-dlist dlist 2)
+    (should (equal (dlist-as-list dlist) '(2 1)))
+
+    (add-to-dlist-at dlist 3 1)
+    (should (equal (dlist-as-list dlist) '(2 3 1)))
+
+    (add-to-dlist-at dlist 4 3)
+    (should (equal (dlist-as-list dlist) '(2 3 1 4)))
+
+    (let ((message "Index out of bounds"))
+      (assert-error (add-to-dlist-at dlist 99 -1) message)
+      (assert-error (add-to-dlist-at dlist 99 5) message))))
+
+(ert-deftest dlist-test-remove-at ()
+  (let ((dlist (make-dlist))
+        (no-such-element "No such element"))
+    (assert-error (remove-from-dlist dlist) no-such-element)
+
     (add-to-dlist dlist 1)
     (add-to-dlist dlist 2)
     (should (equal (remove-from-dlist dlist) 1))
@@ -23,13 +49,39 @@
     (should (equal (remove-from-dlist dlist) 2))
     (should (equal (dlist-as-list dlist) '(3 4)))
 
+    (add-to-dlist dlist 5)
+    (should (equal (remove-from-dlist-at dlist 1) 4))
+    (should (equal (remove-from-dlist-at dlist 1) 5))
+    (should (equal (dlist-as-list dlist) '(3)))
+
+    (assert-error (remove-from-dlist-at dlist 1) no-such-element)
+    (assert-error (remove-from-dlist-at dlist -1) no-such-element)
+
     (remove-from-dlist dlist)
-    (remove-from-dlist dlist)
-    (should-error (remove-from-dlist dlist))))
+    (assert-error (remove-from-dlist dlist) no-such-element)))
+
+(ert-deftest dlist-test-remove-last ()
+  (let ((dlist (make-dlist))
+        (no-such-element "No such element"))
+    (assert-error (remove-last-from-dlist dlist) no-such-element)
+
+    (add-to-dlist dlist 1)
+    (add-to-dlist dlist 2)
+    (should (equal (remove-last-from-dlist dlist) 2))
+    (should (equal (dlist-as-list dlist) '(1)))
+
+    (add-to-dlist dlist 3)
+    (add-to-dlist dlist 4)
+    (should (equal (remove-last-from-dlist dlist) 4))
+    (should (equal (dlist-as-list dlist) '(1 3)))
+
+    (should (equal (remove-last-from-dlist dlist) 3))
+    (should (equal (remove-last-from-dlist dlist) 1))
+    (assert-error (remove-last-from-dlist dlist) no-such-element)))
 
 (ert-deftest dlist-test-clear ()
   (let ((dlist (make-dlist)))
     (add-to-dlist dlist 1)
     (add-to-dlist dlist 2)
-    (clear-dlist dlist)
+    (should (equal (clear-dlist dlist) t))
     (should (equal (dlist-as-list dlist) nil))))
--- emacs-lisp/doubly-linked-list.el
+++ emacs-lisp/doubly-linked-list.el
@@ -11,30 +11,71 @@
     (setf (dnode-next head) tail)
     (make-dlist-0 :head head :tail tail)))
 
+;;; Add
+(defun insert-dnode (dlist element previous next)
+  (let ((node (make-dnode :value element :previous previous :next next)))
+    (setf (dnode-next previous) node)
+    (setf (dnode-previous next) node)
+    node))
+
 (defun add-to-dlist (dlist element)
   "Append to the end."
   (let* ((tail (dlist-tail dlist))
-         (prev (dnode-previous tail))
-         (node (make-dnode :value element :previous prev :next tail)))
-    (setf (dnode-next prev) node)
-    (setf (dnode-previous tail) node)))
+         (prev (dnode-previous tail)))
+    (insert-dnode dlist element prev tail))
+  t)
+
+(defun add-to-dlist-at (dlist element index)
+  (let* ((prev (dlist-head dlist))
+         (next (dnode-next prev))
+         (tail (dlist-tail dlist)))
+    (while (and (> index 0) (not (eq next tail)))
+      (setq prev next)
+      (setq next (dnode-next next))
+      (cl-decf index))
+    (unless (zerop index) (error "Index out of bounds"))
+    (insert-dnode dlist element prev next))
+  t)
+
+(defun add-first-to-dlist (dlist element)
+  (add-to-dlist-at dlist element 0))
+
+;;; Remove
+(defun remove-dnode (dnode)
+  (let ((prev (dnode-previous dnode))
+        (next (dnode-next dnode)))
+    (setf (dnode-next prev) next)
+    (setf (dnode-previous next) prev))
+  (dnode-value dnode))
+
+(defun remove-from-dlist-at (dlist index)
+  (let* ((head (dlist-head dlist))
+         (node (dnode-next head))
+         (tail (dlist-tail dlist)))
+    (while (and (> index 0) (not (eq node tail)))
+      (setq node (dnode-next node))
+      (cl-decf index))
+    (unless (and (zerop index) (not (eq node tail)))
+      (error "No such element"))
+    (remove-dnode node)))
 
 (defun remove-from-dlist (dlist)
   "Remove first."
-  (let* ((head (dlist-head dlist))
-         (node (dnode-next head)))
-    (when (eq node (dlist-tail dlist))
-      (error "List is empty"))
-    (let ((next (dnode-next node)))
-      (setf (dnode-next head) next)
-      (setf (dnode-previous next) head))
-    (dnode-value node)))
+  (remove-from-dlist-at dlist 0))
+
+(defun remove-last-from-dlist (dlist)
+  (let* ((tail (dlist-tail dlist))
+         (node (dnode-previous tail)))
+    (when (eq node (dlist-head dlist))
+      (error "No such element"))
+    (remove-dnode node)))
 
 (defun clear-dlist (dlist)
   (let ((head (dlist-head dlist))
         (tail (dlist-tail dlist)))
     (setf (dnode-next head) tail)
-    (setf (dnode-previous tail) head)))
+    (setf (dnode-previous tail) head))
+  t)
 
 (defun dlist-as-list (dlist)
   (let* ((head (dlist-head dlist))
@@ -51,10 +92,10 @@
   (add-to-dlist dlist 2)
   (remove-from-dlist dlist)
   (add-to-dlist dlist 3)
+  (add-to-dlist-at dlist 4 1)
+  (remove-from-dlist-at dlist 2)
+  (add-first-to-dlist dlist 3)
+  (remove-last-from-dlist dlist)
   (princ (dlist-as-list dlist) t))
 
 (provide 'doubly-linked-list)
-
-;; TODO
-;; Add and remove by index.
-;; Method dispatch. https://nullprogram.com/blog/2018/02/14/
