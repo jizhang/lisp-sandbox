b582440326f85dd2cd22a9844ac4fe865700dc35
Jerry <zhangji87@gmail.com>
2024-12-14 15:24:05 +0800

    Expression with negation

--- emacs-lisp/basic-calculator.el
+++ emacs-lisp/basic-calculator.el
@@ -14,9 +14,8 @@
             (t
              (stack-push s token))))))
 
-(defun basic-calculator-convert-to-rpn (input)
-  (let ((output (make-dlist))
-        (s (make-stack))
+(defun basic-calculator-tokenize (input)
+  (let ((tokens (make-dlist))
         (i 0))
     (while (< i (length input))
       (pcase (aref input i)
@@ -25,45 +24,64 @@
            (while (and (< (1+ i) (length input))
                        (cl-digit-char-p (aref input (1+ i))))
              (setq num (+ (* num 10) (- (aref input (cl-incf i)) ?0))))
-           (add-to-dlist output num)))
+           (add-to-dlist tokens num)))
 
-        ((or ?+ ?- ?* ?/)
-         (let* ((op (intern (char-to-string (aref input i))))
-                (op-precedence (basic-calculator-operator-precedence op)))
-           (while (and (memq (stack-peek s) '(+ - * /))
-                       (>= (basic-calculator-operator-precedence (stack-peek s))
-                           op-precedence))
-             (add-to-dlist output (stack-pop s)))
-           (stack-push s op)))
-
-        (?\(
-         (stack-push s (aref input i)))
-
-        (?\)
-         (while (not (eq (stack-peek s) ?\())
-           (add-to-dlist output (stack-pop s)))
-         (stack-pop s)))
+        ((or ?+ ?- ?* ?/ ?\( ?\))
+         (add-to-dlist tokens (intern (char-to-string (aref input i))))))
 
       (cl-incf i))
+
+    (dlist-as-list tokens)))
+
+(defun basic-calculator-convert-to-rpn (input)
+  (let ((tokens (basic-calculator-tokenize input))
+        (output (make-dlist))
+        (s (make-stack))
+        (previous-token nil))
+    (dolist (token tokens)
+      (cond
+       ((memq token '(+ - * /))
+        (let* ((op (if (basic-calculator-negation-p token previous-token) 'u token))
+               (op-precedence (basic-calculator-operator-precedence op)))
+          (while (and (memq (stack-peek s) '(+ - * / u))
+                      (>= (basic-calculator-operator-precedence (stack-peek s))
+                          op-precedence))
+            (add-to-dlist output (stack-pop s)))
+          (stack-push s op)))
+
+       ((eq token '\()
+        (stack-push s token))
+
+       ((eq token '\))
+        (while (not (eq (stack-peek s) '\())
+          (add-to-dlist output (stack-pop s)))
+        (stack-pop s))
+
+       (t
+        (add-to-dlist output token)))
+
+      (setq previous-token token))
 
     (while (> (stack-size s) 0)
       (add-to-dlist output (stack-pop s)))
 
     (dlist-as-list output)))
 
+(defun basic-calculator-negation-p (op previous-token)
+  (and (eq op '-)
+       (or (null previous-token)
+           (memq previous-token '(+ - * / \()))))
+
 (defun basic-calculator-operator-precedence (op)
   (cond ((memq op '(+ -)) 10)
         ((memq op '(* /)) 20)
+        ((memq op '(u)) 30)
         (t 0)))
 
 (defun basic-calculator (input)
   (let ((tokens (basic-calculator-convert-to-rpn input)))
-    (princ tokens t) ; FIXME
     (reverse-polish-notation tokens)))
 
-(princ (reverse-polish-notation '(1 2 u +)) t)
-
-;; TODO
-;; Negative expressions: -10 + 1, -(10 + 1)
+(princ (basic-calculator-convert-to-rpn "-1") t)
 
 (provide 'basic-calculator)
